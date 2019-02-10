2c895c9edaccc9e7b4f8eaf9d4df6aa10a2a83d3
sNessie <a.czajczyk@icloud.com>
2019-02-10 16:47:32 +0100

    redux - add action add_habit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import './index.css';
 import App from './routers/App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore} from 'redux';
+import { createStore } from 'redux';
 import rootReducers from './reducers/rootReducers';
+import {addHabit } from './reducers/habitsReducers';
 
 
 
 const store = createStore(rootReducers);
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+  });
 
+store.dispatch(addHabit({name: "walk a dog", date: '2019-02-01'}));
+store.dispatch(addHabit({name: "jogging", date: '2019-02-09'}));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
--- src/reducers/habitsReducers.js
+++ src/reducers/habitsReducers.js
@@ -1,10 +1,35 @@
+import uuid from 'uuid';
+
 const habitsDefaultState = [];
+
+export const addHabit = (
+  {
+    name = '', 
+    date = '', 
+    times = 0
+  } = {}
+  ) => ({
+  type: 'ADD_HABIT', 
+  habit: {
+    id:uuid(), 
+    name, 
+    date, 
+    times
+  }
+});
 
 const habitsReducers = (state = habitsDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_HABIT':
+      return [
+        ...state, 
+        action.habit
+      ];
     default:
       return state;
   }
 };
+
+
 
 export default habitsReducers;

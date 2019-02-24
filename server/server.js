17f56599907484a49235981401330ccc87ffdf0e
sNessie <a.czajczyk@icloud.com>
2019-02-24 21:46:31 +0100

    heroku deploy

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(publicPath));
+app.listen(port, ()=> {
+  console.log('server is up');
+}); // which port should node use to setup server
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+}) // this fn made it is possible to go direct to site (without this is become error  )

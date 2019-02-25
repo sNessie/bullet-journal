a2a6ce3dca626ae8ada06f0546c3a17269b71651
sNessie <agnescz@outlook.com>
2019-02-25 11:51:08 +0100

    add first data to Firebase

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 
  // Initialize Firebase
  const config = {
@@ -12,4 +13,15 @@ import firebase from 'firebase/app';
 
 firebase.initializeApp(config);
 
+const database = firebase.database();
 
+database.ref().set({
+    name: 'walk a dog',
+    date: '2019-02-01',
+    times: 1,
+    timesRepeat: [{
+        "id": 101,
+        "date":"2018-09-05",
+        "ready": false
+        }]
+})

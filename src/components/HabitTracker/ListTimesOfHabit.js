b65a4252c10386096f1536e88ef29244530c32bd
sNessie <agnescz@outlook.com>
2019-04-15 10:27:00 +0200

    rewrite as Statless component + propTypes

--- src/components/HabitTracker/ListTimesOfHabit.js
+++ src/components/HabitTracker/ListTimesOfHabit.js
@@ -1,44 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { startMakeHabit } from "../../reducers/habitsReducers";
 import { visibleHabits } from "../../reducers/rootReducers";
 import { Row, Button, Col } from "react-materialize";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 import "./style.css";
 
-class ListTimesOfHabit extends Component {
-  constructor(props) {
-    super(props);
-  }
-  handleClick = (id, key) => {
-    this.props.dispatch(startMakeHabit(id, key));
+const ListTimesOfHabit = ({ habitId, times, actions }) => {
+  const handleClick = (id, key) => {
+    actions.startMakeHabit(id, key);
   };
-  render() {
-    const timesList = this.props.times.map(time => {
-      return (
-        <Col s={2} key={time.id} className="habit">
-          <span className="hidden">{time.date}</span>
-          <Button
-            floating
-            small="true"
-            className="red"
-            waves="light"
-            icon={time.ready ? "done" : "done_outline"}
-            disabled={time.ready ? true : false}
-            onClick={() => this.handleClick(time.id, this.props.habitId)}
-          />
-        </Col>
-      );
-    });
-    return <Row>{timesList}</Row>;
-  }
-}
+  const timesList = times.map(time => {
+    return (
+      <Col s={2} key={time.id} className="habit">
+        <span className="hidden">{time.date}</span>
+        <Button
+          floating
+          small="true"
+          className="red"
+          waves="light"
+          icon={time.ready ? "done" : "done_outline"}
+          disabled={time.ready ? true : false}
+          onClick={() => handleClick(time.id, habitId)}
+        />
+      </Col>
+    );
+  });
+  return <Row>{timesList}</Row>;
+};
 
-ListTimesOfHabit.propTypes = {};
+ListTimesOfHabit.propTypes = {
+  times: PropTypes.array.isRequired,
+  habitId: PropTypes.string.isRequired,
+  actions: PropTypes.object.isRequired
+};
 
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: {
+      startMakeHabit: bindActionCreators(startMakeHabit, dispatch)
+    }
+  };
+};
 const mapStateToProps = state => {
   return {
     habits: visibleHabits(state.habits, state.filters)
   };
 };
-export default connect(mapStateToProps)(ListTimesOfHabit);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ListTimesOfHabit);

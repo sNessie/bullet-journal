50a9ffad2b0817bdab20cc5bbc518bb34ef96503
sNessie <agnescz@outlook.com>
2019-04-19 15:58:33 +0200

    rewrite HabitTracker component as Hooks

--- src/components/HabitTracker/HabitTracker.js
+++ src/components/HabitTracker/HabitTracker.js
@@ -1,61 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import AddHabit from "./AddHabit";
-import uuid from "uuid";
 import ListHabits from "./ListHabits";
 import FiltersForm from "./FiltersForm";
 import { Button, Row, Col } from "react-materialize";
 
-class HabitTracker extends Component {
-  state = {
-    showAddForm: false
-  };
-  showForm = () => {
-    this.setState(prevState => {
-      return {
-        showAddForm: !prevState.showAddForm
-      };
-    });
-  };
-
-  handleSave = habit => {
-    this.setState(() => {
-      const newHabit = { ...habit, id: uuid() };
-      return {
-        habits: [...this.state.habits, newHabit]
-      };
-    });
-  };
-
-  makeReady = i => {
-    this.setState(() => {
-      const readyHabit = this.state.habits.find(h =>
-        h.timesRepeat.find(t => t.id === i)
-      );
-      const readyDayHabit = readyHabit.timesRepeat.find(t => t.id === i);
-      readyDayHabit.ready = !readyDayHabit.ready;
-      return {
-        habits: [...this.state.habits]
-      };
-    });
-  };
-
-  render() {
-    const { showAddForm } = this.state;
-    return (
-      <Row className="container">
-        <Row>
-          <FiltersForm />
-          <Col className="right">
-            <Button waves="light" onClick={this.showForm}>
-              {showAddForm ? "Hide add form" : "Show add form"}
+const HabitTracker = () => {
+  const [showAddForm, setShowAddForm] = useState(false);
+  return (
+    <Row className="container">
+      <Row>
+        <FiltersForm />
+        <Col className="right">
+          {showAddForm ? (
+            <Button waves="light" onClick={() => setShowAddForm(false)}>
+              Hide add form
             </Button>
-          </Col>
-        </Row>
-        {showAddForm ? <AddHabit showForm={this.showForm} /> : null}
-        <ListHabits />
+          ) : (
+            <Button waves="light" onClick={() => setShowAddForm(true)}>
+              Show add form
+            </Button>
+          )}
+        </Col>
       </Row>
-    );
-  }
-}
+      {showAddForm ? <AddHabit showForm={showAddForm} /> : null}
+      <ListHabits />
+    </Row>
+  );
+};
 
 export default HabitTracker;

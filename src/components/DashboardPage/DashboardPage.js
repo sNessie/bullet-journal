db5e5b885e63496c62c50694339a742601228de4
sNessie <agnescz@outlook.com>
2019-04-20 23:00:09 +0200

    added styled components & globalStyle

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Montserrat');
+    font-family: 'Montserrat', sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const DashboardPage = () => (
-  <div>
-    Dashboad Page
-  </div>
+  <>
+    <GlobalStyle />
+    <StyledWrapper>Dashboad Page</StyledWrapper>
+  </>
 );
 
 export default DashboardPage;
--- src/components/HabitTracker/FiltersForm.js
+++ src/components/HabitTracker/FiltersForm.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { setTextFilter } from "../../reducers/filtersReducers";
-import { Input, Icon } from "react-materialize";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
@@ -9,17 +8,7 @@ const FiltersForm = ({ filters, actions }) => {
   const handleChange = e => {
     actions.setTextFilter(e.target.value);
   };
-  return (
-    <Input
-      s={4}
-      label="Search"
-      type="text"
-      value={filters.text}
-      onChange={handleChange}
-    >
-      <Icon>search</Icon>
-    </Input>
-  );
+  return <input type="text" value={filters.text} onChange={handleChange} />;
 };
 
 FiltersForm.propTypes = {
